4c7a2ec0812357fb8d7b7ad64c16e5781e2b1382
Andrey Gubarev <andrey@andreygubarev.com>
2023-03-10 00:33:54 +0400

    add deps

--- providers.tf
+++ providers.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+
+    cloudinit = {
+      source = "hashicorp/cloudinit"
+      version = "~> 2.0"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
